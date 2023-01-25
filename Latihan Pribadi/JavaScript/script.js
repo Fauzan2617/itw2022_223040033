09c9281b08508482d364c27c7f8fe9d279578e89
Fauzan2617 <mfauzandwiputera10@gmail.com>
2023-01-25 13:15:20 +0700

    Belajar Materi popup JS

---
+++ Latihan Pribadi/JavaScript/script.js
@@ -0,0 +1,21 @@
+// Materi Var
+var x = "10";
+console.log("fauzan");
+console.log("fauzan" + x);
+
+// Materi Popup Box
+// Alert
+// alert("Woii Sahabat");
+// alert("assallammulaikum");
+// alert("asiiappppp");
+
+// Prompt
+// prompt("Passwordnya kakak :");
+// Jika menambah sebuah variabel contohnya nama, maka ketika memanggil var nama akan muncul sesuai yang diisikan
+// var nama = prompt("username:");
+// alert(nama);
+
+// confirm
+// confirm("yakin dekk??");
+var kondisi = confirm("yakinn dekk ??");
+alert(kondisi);
